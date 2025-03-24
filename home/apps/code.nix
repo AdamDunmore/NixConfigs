83eb0d90cb2f5304f4ab41d62f4bf02602db726e
AdamDunmore <adamfdunmore@gmail.com>
2025-03-24 22:47:14 +0000

    [Home Manager] Updated vscode for new options

--- home/apps/code.nix
+++ home/apps/code.nix
@@ -7,33 +7,35 @@ with lib;
     config = mkIf (cfg == "light" || cfg == "all") {
         programs.vscode = {
             enable = true;
-            enableUpdateCheck = false;
-            mutableExtensionsDir = false;
-            extensions = with pkgs.vscode-extensions; [
-                vscodevim.vim
-                vue.volar
-                golang.go
-                jnoortheen.nix-ide
-                ms-python.python
-                ritwickdey.liveserver
-                ms-vsliveshare.vsliveshare
-            ];
-            languageSnippets = {
-                javascript = {
-                    log = {
-                        body = [
-                            "console.log($0);"
-                        ];
-                        description = "Inserts console.log()";
-                        prefix = [
-                            "log"
-                        ];
+            profiles.default = {
+                extensions = with pkgs.vscode-extensions; [
+                    vscodevim.vim
+                    vue.volar
+                    golang.go
+                    jnoortheen.nix-ide
+                    ms-python.python
+                    ritwickdey.liveserver
+                    ms-vsliveshare.vsliveshare
+                ];
+                enableUpdateCheck = false;
+                userSettings = {
+                    "files.autoSave" = "off";
+                };
+                languageSnippets = {
+                    javascript = {
+                        log = {
+                            body = [
+                                "console.log($0);"
+                            ];
+                            description = "Inserts console.log()";
+                            prefix = [
+                                "log"
+                            ];
+                        };
                     };
                 };
             };
-            userSettings = {
-                "files.autoSave" = "off";
-            };
+            mutableExtensionsDir = false;
         };
     };
 }
