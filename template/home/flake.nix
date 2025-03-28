e200db34a5857b894adbf5fe5154b0bfc386d784
AdamDunmore <adamfdunmore@gmail.com>
2025-03-28 14:19:02 +0000

    [Home manager] More work on templates

--- template/home/flake.nix
+++ template/home/flake.nix
@@ -2,11 +2,33 @@
     description = "Config for home manager";
 
     inputs = {
-        conf = "github:AdamDunmore/NixConfigs";
+        conf.url = "github:AdamDunmore/NixConfigs";
     };
 
-    outputs = { conf }: {
-        nixosConfigurations.default = conf.nixosConfigurations.default;
+    outputs = { conf, self, ... }:
+    let
+        local = {
+            username = "user";
+            system = "x86_64-linux";
+            stable_version = "24.11";
+        };
+    in
+    {
+        homeConfigurations."${local.username}" = inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home ];
+            extraSpecialArgs = {
+                inherit local;
+                inherit inputs;
+            };
+        };
+        packages."${local.system}".hm_conf = self.homeConfigurations."${local.username}".activationPackage;
+ 
+        # sudo nix run .#default
+        apps."${local.system}".default = {
+            type = "app";
+            program = "${self.packages."${local.system}".hm_conf}/activate"; 
+        };
     };
 
 }
