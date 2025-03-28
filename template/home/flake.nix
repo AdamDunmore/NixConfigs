c7fa8deba9de36ad4888ed1047f25590d426b0c4
AdamDunmore <adamfdunmore@gmail.com>
2025-03-28 14:08:48 +0000

    [Home manager] Added hm template

--- template/home/flake.nix
+++ template/home/flake.nix
@@ -1,5 +1,12 @@
 {
+    description = "Config for home manager";
+
     inputs = {
         conf = "github:AdamDunmore/NixConfigs";
     };
+
+    outputs = { conf }: {
+        nixosConfigurations.default = conf.nixosConfigurations.default;
+    };
+
 }
