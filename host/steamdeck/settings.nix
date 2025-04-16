cf45e43d3d852365677c1456b567ee7cb362d20a
Adam Dunmore <adamfdunmore@gmail.com>
2025-04-16 03:08:56 +0100

    [Steamdeck] Disabled unneeded modules for host

---
+++ host/steamdeck/settings.nix
@@ -0,0 +1,15 @@
+{ lib, ... }:
+{
+    config.settings = {
+        home = {
+            apps = {
+                autostart.enable = false;
+                level = lib.mkForce "minimal";
+            };
+            scripts = false;
+            terminal.terminals.alacritty = true;
+            theme = false;
+        };
+        nixos.keyboard.custom_layout = false;
+    };
+}
