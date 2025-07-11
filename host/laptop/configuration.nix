be82e9bc2ddaa837684cfd38890a23fa4390a7c8
Adam Dunmore <adamfdunmore@gmail.com>
2025-07-11 06:57:25 +0100

    [Nixos] Moved fprint to laptop module

--- host/laptop/configuration.nix
+++ host/laptop/configuration.nix
@@ -1,4 +1,17 @@
 {
     networking.hostName = "laptop";
     boot.loader.grub.efiSupport = true;
+
+    # Sets up fprint
+    services.fprintd = {
+        enable = true;
+    };
+
+    
+    security.pam.services = {
+        login.fprintAuth = true;  
+        # sudo.fprintAuth = true;
+        sddm.fprintAuth = true;      
+        hyprlock.fprintAuth = true;  
+    };
 }
