5f2c6aaa0cd24237e0db4756a0324a45bd37e4c1
Adam Dunmore <adamfdunmore@gmail.com>
2025-07-12 16:14:48 +0100

    [Home] More ags work

--- home/widgets/ags/sidebar.tsx
+++ home/widgets/ags/sidebar.tsx
@@ -1,4 +1,5 @@
 import { Astal } from "ags/gtk4"
+import { execAsync } from "ags/process"
 import Mpris from "gi://AstalMpris"
 
 const { TOP, RIGHT, BOTTOM } = Astal.WindowAnchor
@@ -6,10 +7,17 @@ const { TOP, RIGHT, BOTTOM } = Astal.WindowAnchor
 export default function Sidebar(){
     return (
         <window visible anchor={TOP | RIGHT | BOTTOM }>
-            <box>Hello World</box>
-            <button valign = "end" onClicked={(self) => {}}>
-                <label label="L" />
-            </button>
+            <box>
+                <button onClicked={(self) => { execAsync("hyprlock") }}>
+                    <label label="L" />
+                </button>
+                <button onClicked={(self) => { execAsync("systemctl suspend") }}>
+                    <label label="S" />
+                </button>
+                <button onClicked={(self) => { execAsync("shutdown now") }}>
+                    <label label="P" />
+                </button>
+            </box>
         </window>
     )
 }
