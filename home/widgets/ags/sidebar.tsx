0af7f7cf4ba66e30088a7db747964aa24a24e215
Adam Dunmore <adamfdunmore@gmail.com>
2025-07-13 10:11:54 +0100

    [Home] ags work

--- home/widgets/ags/sidebar.tsx
+++ home/widgets/ags/sidebar.tsx
@@ -8,19 +8,24 @@ const { TOP, RIGHT, BOTTOM } = Astal.WindowAnchor
 export default function Sidebar(){
     return (
         <window visible={false} name="sidebar" $={(self) => app.add_window(self)} anchor={TOP | RIGHT | BOTTOM }>
-            <box class="power_box" valign="2">
-                <button onClicked={(self) => { execAsync("hyprlock") }} class="power_button">
-                    <label label=""/>
-                </button>
-                <button onclicked={(self) => { execAsync("systemctl suspend") }} class="power_button">
-                    <label label="󰤄"/>
-                </button>
-                <button onclicked={(self) => { execAsync("reboot") }} class="power_button">
-                    <label label="󰜉"/>
-                </button>
-                <button onClicked={(self) => { execAsync("shutdown now") }} class="power_button">
-                    <label label="⏻"/>
-                </button>
+            <box class="container">
+                <box class="mpris_box" valign="1">
+                    <label label="mpris"/>
+                </box>
+                <box class="power_box" valign="2">
+                    <button onClicked={(self) => { execAsync("hyprlock") }} class="power_button">
+                        <label label=""/>
+                    </button>
+                    <button onclicked={(self) => { execAsync("systemctl suspend") }} class="power_button">
+                        <label label="󰤄"/>
+                    </button>
+                    <button onclicked={(self) => { execAsync("reboot") }} class="power_button">
+                        <label label="󰜉"/>
+                    </button>
+                    <button onClicked={(self) => { execAsync("shutdown now") }} class="power_button">
+                        <label label="⏻"/>
+                    </button>
+                </box>
             </box>
         </window>
     )
