9ecee89dcb0e87c9ed8d1e5ad4698b0ac740b7e8
Adam Dunmore <adamfdunmore@gmail.com>
2025-08-12 09:30:53 +0100

    [Nixos] Added nixos-hardware framework module

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         nixpkgs-24_11.url = "github:nixos/nixpkgs/nixos-24.11";
 
+        nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
         home-manager.url = "github:nix-community/home-manager";
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
--- host/laptop/configuration.nix
+++ host/laptop/configuration.nix
@@ -1,34 +1,40 @@
-{ pkgs, ... }: 
+{ pkgs, inputs, ... }: 
 
 {
-    networking.hostName = "laptop";
-    boot.loader.grub.efiSupport = true;
+    imports = [ inputs.nixos-hardware.nixosModules.framework-13-7040-amd ];
 
-    # Sets up fprint
-    services.fprintd = {
-        enable = true;
-    };
+    config = {
+        networking.hostName = "laptop";
+        boot.loader.grub.efiSupport = true;
 
-    # Enables AMD GPU drivers
-    boot.initrd.kernelModules = [ "amdgpu" ];
-    services.xserver.videoDrivers = [ "amdgpu" ];
-    hardware = {
-        graphics = {
+        hardware.framework.enableKmod = true;
+
+        # Sets up fprint
+        services.fprintd = {
+            enable = true;
+        };
+
+        security.pam.services = {
+            login.fprintAuth = true;  
+            # sudo.fprintAuth = true;
+            sddm.fprintAuth = true;      
+            hyprlock.fprintAuth = true;  
+        };
+
+        # Enables AMD GPU drivers
+        hardware.amdgpu.initrd.enable = true;
+        boot.initrd.kernelModules = [ "amdgpu" ];
+        services.xserver.videoDrivers = [ "amdgpu" ];
+        hardware.graphics = {
             enable = true;
             extraPackages = with pkgs; [
+                amdvlk
+                mesa
                 vulkan-loader
                 vulkan-validation-layers
                 vulkan-extension-layer
             ];
             enable32Bit = true; # Needed for steam
         };
-    };
-
-    
-    security.pam.services = {
-        login.fprintAuth = true;  
-        # sudo.fprintAuth = true;
-        sddm.fprintAuth = true;      
-        hyprlock.fprintAuth = true;  
     };
 }
