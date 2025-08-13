885c724a31663ed13b9a381bd7871f425226afb4
Adam Dunmore <adamfdunmore@gmail.com>
2025-08-13 09:22:11 +0100

    [Nixos] Added phone to syncthing

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -18,12 +18,13 @@ in
                     # "server" = { id = ""; };
                     "desktop" = { id = "HORZPBB-TUIKUFE-XPNPGUP-ISUS6J7-3VVS6AW-HGJCRBA-CB67AHJ-4UDPQAF"; };
                     "laptop" = { id = "KIPS7XQ-OJUQDR7-QYSLXWJ-YMOM7NZ-H7PNGSK-V323JHN-EUEO7U2-MP43WQZ"; };
+                    "phone" = { id = "4SM6XJW-PAY7RDD-LJN5QMT-UVNLYMA-UETJG7G-KNVZ4HP-7TJ4IKX-LKMILAP"; };
                 };
 
                 folders = {
                     "Music" = {
                         path = "/home/${local.username}/Music";
-                        devices = [ "desktop" "laptop" ]; #"server"
+                        devices = [ "desktop" "laptop" "phone" ]; #"server"
                         ignorePerms = false;
                     };
                 };
