076015c083bef7e4014c758794676caadc6a80c8
Adam Dunmore <adamfdunmore@gmail.com>
2025-08-22 06:24:58 +0100

    [Nixos] Added folder definitions

--- host/server/configuration.nix
+++ host/server/configuration.nix
@@ -4,15 +4,21 @@
     config = {
         networking.hostName = "server";
 
-	boot.loader.grub.enable = true;
-	boot.loader.grub.device = "/dev/sda";
+	    boot.loader.grub.enable = true;
+	    boot.loader.grub.device = "/dev/sda";
 	
-	services.openssh.enable = true;
-	services.openssh.settings.PermitRootLogin = "yes";
+	    services.openssh.enable = true;
+	    services.openssh.settings.PermitRootLogin = "yes";
 
-	services.jellyfin = {
-	  enable = true;
-	};
-	networking.firewall.allowedTCPPorts = [ 8096 ];
+	    services.jellyfin = {
+	        enable = true;
+	    };
+	    networking.firewall.allowedTCPPorts = [ 8096 ];
+
+        systemd.tmpfiles.rules = [
+            "d /mnt/MediaDrive/Music/ 0777 root root -"
+            "d /mnt/MediaDrive/Work/ 0777 root root -"
+            "d /mnt/MediaDrive/Movies/ 0777 root root -"
+        ];
     };
 }
