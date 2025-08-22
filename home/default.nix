76e92398c9848b764024a84b171a781f8a494feb
Adam Dunmore <adamfdunmore@gmail.com>
2025-08-22 05:22:52 +0100

    [Home] Removed xdg dirs on minimal systems

--- home/default.nix
+++ home/default.nix
@@ -1,12 +1,14 @@
-{ local, lib, host, ... }:
-with lib;
+{ local, lib, host, config, ... }:
+let
+    inherit (lib) mkIf mkForce;
+in
 {
     config = {
         home.username = mkForce local.username; 
         home.homeDirectory = mkForce "/home/${local.username}";
         home.stateVersion = mkForce local.stable_version;
 
-        xdg.userDirs = {
+        xdg.userDirs = mkIf (config.settings.home.apps.level == "minimal") {
             enable = true;
             createDirectories = true;
         };
