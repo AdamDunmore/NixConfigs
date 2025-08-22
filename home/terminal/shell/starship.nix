00b0cc1f4bed15cde7a25699c1283b5624e6012d
Adam Dunmore <adamfdunmore@gmail.com>
2025-08-22 05:57:49 +0100

    [Home] Changed starship prompt

--- home/terminal/shell/starship.nix
+++ home/terminal/shell/starship.nix
@@ -10,13 +10,18 @@ with lib;
             enable = true;
             enableZshIntegration = true;
             settings = {
-                format = "[ ](${colours.light_blue.one})$username[](fg:${colours.light_blue.one} bg:${colours.light_blue.two})$directory[ ](${colours.light_blue.two})";
+                format = "[ ](${colours.light_blue.one})$username[](fg:${colours.light_blue.one} bg:${colours.blue.one})$hostname[](fg:${colours.blue.one} bg:${colours.light_blue.two})$directory[ ](${colours.light_blue.two})";
                 add_newline = false;
                 username = {
                     style_user = "bg:${colours.light_blue.one} fg:${colours.white.one}";
                     disabled = false;
                     show_always = true;
                     format = "[$user ]($style)";
+                };
+                hostname = {
+                    format = "[ $hostname ]($style)"; 
+                    style = "bg:${colours.blue.one} fg:${colours.white.one}";
+                    ssh_only = true;
                 };
                 directory = {
                     format = "[ $path ]($style)";
