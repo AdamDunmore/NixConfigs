ed922bab511424236fd88bb78ab5b1386eb668ae
Adam Dunmore <adamfdunmore@gmail.com>
2025-09-25 13:24:33 +0100

    [Nixos] Polished nginx module

--- nixos/services/nginx.nix
+++ nixos/services/nginx.nix
@@ -10,15 +10,16 @@ in
             enable = true;
             virtualHosts."dav.local" = {
                 listen = [ { addr = "0.0.0.0"; port = 8080; } ];
-                root = "/";
                 locations = {
-                    "/share/music".extraConfig = ''
-                        root ${music-path};
-                        dav_methods     PUT DELETE MKCOL COPY MOVE;
-                        dav_ext_methods PROPFIND OPTIONS;
-                        create_full_put_path on;
-                        autoindex on;
-                    '';
+                    "/music/" = {
+                        alias = "${music-path}/";
+                        extraConfig = ''
+                            dav_methods     PUT DELETE MKCOL COPY MOVE;
+                            dav_ext_methods PROPFIND OPTIONS;
+                            create_full_put_path on;
+                            autoindex on;
+                        '';
+                    };
                 };
             };
         };
