654fa8f07aee8f0f0215a404a6208306103d023b
Adam Dunmore <adamfdunmore@gmail.com>
2025-12-10 05:10:06 +0000

    [Home] Updated sink cycle script

--- home/scripts/scripts/sinkcycle.sh
+++ home/scripts/scripts/sinkcycle.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env sh
 
-sinks=($(pactl list short sinks | cut -f1))
-current_sink=$(pactl info | grep "Default Sink" | awk '{print $3}')
+# Get list of ALL sink names in correct order
+mapfile -t sinks < <(pactl list short sinks | awk '{print $2}')
 
-# Get index of current sink
+# Get current sink name
+current_sink=$(pactl get-default-sink)
+
+# Find current sink's index
 current_index=-1
 for i in "${!sinks[@]}"; do
-  if pactl list short sinks | grep -q "${sinks[i]}.*${current_sink}"; then
-    current_index=$i
-    break
-  fi
+    if [[ "${sinks[$i]}" == "$current_sink" ]]; then
+        current_index=$i
+        break
+    fi
 done
 
-# Compute next sink index (wrap around)
+# If not found (rare), start from 0
+if [[ "$current_index" -lt 0 ]]; then
+    current_index=0
+fi
+
+# Compute next sink
 next_index=$(( (current_index + 1) % ${#sinks[@]} ))
 next_sink="${sinks[$next_index]}"
 
-# Set new default sink
+# Apply new sink
 pactl set-default-sink "$next_sink"
 
-# Move all playing streams to the new sink
-for input in $(pactl list short sink-inputs | cut -f1); do
-  pactl move-sink-input "$input" "$next_sink"
+# Move all current streams
+for input in $(pactl list short sink-inputs | awk '{print $1}'); do
+    pactl move-sink-input "$input" "$next_sink"
 done
 
