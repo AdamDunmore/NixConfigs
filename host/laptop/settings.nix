870bb3b5a7ab4f2688acd6ddd2463baaa3373073
Adam Dunmore <adamfdunmore@gmail.com>
2026-03-12 21:52:29 +0000

    [Home] Disabled cosmic so default boot is sway

--- host/laptop/settings.nix
+++ host/laptop/settings.nix
@@ -2,6 +2,6 @@
 {
     config.settings = {
         home.apps.level = lib.mkForce "all";
-        home.wm.cosmic.enable = true;
+        home.wm.cosmic.enable = false; # TODO cant see mouse in sddm so this it my only way to get sway
     };
 }
