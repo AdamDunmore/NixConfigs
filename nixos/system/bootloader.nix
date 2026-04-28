7f6fbfedb81338136e4c3b3a258359f5c3a1c06b
Adam Dunmore <adamfdunmore@gmail.com>
2026-04-28 01:55:10 +0100

    [Nixos] Changed bootscreen

--- nixos/system/bootloader.nix
+++ nixos/system/bootloader.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 let
     cfg = config.settings.nixos.system.bootloader;
 in
@@ -9,12 +9,22 @@ in
             timeout = 3;
             grub = {
                 enable = true;
-                backgroundColor = "#2E3440";
+                # backgroundColor = "#2E3440"; # TODO figure out why not fullscreen colour
+                theme = pkgs.minimal-grub-theme; 
                 device = "nodev";
                 useOSProber = true;
                 efiInstallAsRemovable = false;
             };
             systemd-boot.enable = false;   
+        };
+
+        # Custom boot screen
+        boot.initrd.systemd.enable = true;
+        boot.kernelParams = [ "quiet" "splash" "loglevel=3" ];
+        boot.plymouth = {
+          enable = true;
+            theme = "fade-in";
+            logo = "${pkgs.nixos-icons}/share/icons/hicolor/256x256/apps/nix-snowflake.png";
         };
     };
 }
