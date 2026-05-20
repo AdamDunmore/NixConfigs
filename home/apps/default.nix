1e5147dc240858b546b9faf4ec6e104bcc637d28
Adam Dunmore <adamfdunmore@gmail.com>
2026-05-20 23:17:58 +0100

    [Home] Added stremio

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -56,6 +56,7 @@ in
                 proton-pass
                 protonmail-desktop
                 proton-vpn
+                stremio-linux-shell
 
                 qemu
                 quickgui
