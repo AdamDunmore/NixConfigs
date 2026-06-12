c1a2970fbbe7c7730d30ade7cab9cdf5cf9141ec
Adam Dunmore <adamfdunmore@gmail.com>
2026-06-12 01:04:31 +0100

    [Flake] Updated Inputs

--- utils/pkgs.nix
+++ utils/pkgs.nix
@@ -4,6 +4,7 @@ import nixpkgs {
     config = {
         permittedInsecurePackages = [
             "electron-27.3.11"
+            "electron-39.8.10"
         ];
         allowUnfree = true;
         allowBroken = true;
