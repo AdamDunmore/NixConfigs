f86295ee712b4a680b9e1945c58a75dc31472ca0
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-05 18:55:53 +0100

    [Host] Enabled rocm for desktop adam

--- users/adam/hosts/desktop/settings.nix
+++ users/adam/hosts/desktop/settings.nix
@@ -1,10 +1,25 @@
+{ lib, ... }:
+let
+    inherit (lib) mkForce;
+in
 {
     imports = [
         ../../settings.nix
     ];
     config = {
-        settings.values.primary-monitor = "DP-2";
-        settings.modules.home.wm.replays = true;
-        settings.modules.nixos.services.ai.enable = true;
+        settings = {
+            values.primary-monitor = "DP-2";
+            modules = {
+                home = {
+                    wm.replays = true;
+                };
+                nixos = {
+                    services.ai = {
+                        enable = mkForce true;
+                        enableRocm = true; 
+                    };
+                };
+            };
+        };
     };
 }
