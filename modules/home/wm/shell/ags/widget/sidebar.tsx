600d22d05cee6c0f7e16b3e115d1818e25f8b9a9
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-10 12:11:30 +0100

    [AGS] Fixed menu sizing

--- modules/home/wm/shell/ags/widget/sidebar.tsx
+++ modules/home/wm/shell/ags/widget/sidebar.tsx
@@ -26,11 +26,11 @@ export default function Sidebar(){
         <window visible={isWindowVisible(v => v)} name="sidebar" $={(self) => app.add_window(self)} anchor={TOP | RIGHT | BOTTOM } keymode={Astal.Keymode.ON_DEMAND}>
             <revealer class = "window" revealChild={isVisible(v => v)} transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT} transitionDuration={400} vexpand>
                 <box vexpand spacing={5} orientation={Gtk.Orientation.HORIZONTAL} homogeneous>
-                    <box vexpand orientation={Gtk.Orientation.VERTICAL} spacing={5} homogeneous>
+                    <box vexpand orientation={Gtk.Orientation.VERTICAL} spacing={5} homogeneous class="col_2">
                         <Calendar />
                         <MprisItem />
                     </box>
-                    <box vexpand orientation={Gtk.Orientation.VERTICAL} spacing={5}>
+                    <box vexpand orientation={Gtk.Orientation.VERTICAL} spacing={5} class="col_1">
                         <Menu />
                         <AppMenu app_visible={isVisible} close={() => {toggle_app(isWindowVisible, setIsWindowVisible, setIsVisible)}} show_app={() => { setIsVisible(true); setIsWindowVisible(true) }}/>
                     </box>
