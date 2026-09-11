c856e38131d61f7b110fa7e00c01f853575175b4
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-12 00:51:14 +0100

    [Home] Overrides gtk theme with colours

--- modules/home/wm/theme/gtk.nix
+++ modules/home/wm/theme/gtk.nix
@@ -1,18 +1,32 @@
 { pkgs, lib, config, ... }:
 let
     font = config.settings.values.font;
-    cfg = config.settings.modules.home.wm.theme.gtk;
+    cfg = config.settings.modules.home.wm.theme.gtk; 
+    colours = config.settings.values.colours;
+    css = ''
+      @define-color theme_bg_color ${colours.bg};
+      @define-color theme_selected_bg_color ${colours.bg_selected};
+      @define-color theme_fg_color ${colours.fg};
+      @define-color theme_text_color ${colours.fg};
+      @define-color theme_selected_fg_color ${colours.fg_selected};
+
+      @define-color theme_base_color ${colours.base};
+      @define-color borders ${colours.border};
+
+    '';
     inherit (lib) mkIf;
 in
 {
     config = mkIf cfg.enable {
         gtk = {
             enable = true;
+            gtk3.extraCss = mkIf cfg.overrideTheme css;
+            gtk4.extraCss = mkIf cfg.overrideTheme css;
+            gtk4.theme = config.gtk.theme;
             theme = {
                 name = "Nordic";
                 package = pkgs.nordic;
             };
-            gtk4.theme = config.gtk.theme;
             iconTheme = {
                 name = "Papirus-Dark";
                 package = pkgs.papirus-icon-theme;
--- options/modules/home/wm/theme/gtk.nix
+++ options/modules/home/wm/theme/gtk.nix
@@ -10,5 +10,11 @@ in
             example = false;
             description = "Enables the gtk module";
         };
+        overrideTheme = mkOption {
+            type = lib.types.bool;
+            default = config.settings.modules.home.wm.theme.gtk.enable;
+            example = false;
+            description = "Overrides the gtk theme with config.values.colours";
+        };
     };
 }
