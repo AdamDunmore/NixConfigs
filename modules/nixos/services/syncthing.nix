52dbcb758eed9b1687215aa731bf43d4d6c7eeba
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-12 09:18:14 +0100

    [Nixos] Updated server syncthing id

--- modules/nixos/services/syncthing.nix
+++ modules/nixos/services/syncthing.nix
@@ -19,7 +19,7 @@ in
                     "desktop" = { id = "DIZXZOG-IIRYVVX-F2IYNJH-HD6TZNG-VCKQULR-PVNGL7Z-7HMA4KN-ZF66RAU"; };
                     "laptop" = { id = "KIPS7XQ-OJUQDR7-QYSLXWJ-YMOM7NZ-H7PNGSK-V323JHN-EUEO7U2-MP43WQZ"; };
                     "phone" = { id = "5ZJWJJU-UNRIIOM-FAOAEI3-SFDYZRS-KL64MUW-PUVDXYX-POIUTLP-B3VH3AG"; };
-                    "server" = { id = "YOPRCA6-AFYUSFG-IWDAO5P-FD22Q2Z-QACENI2-QADPSNH-UDJR36C-PYHBUQC"; };
+                    "server" = { id = "LKRBLKJ-PV5UKXZ-SGNUGIQ-X4MRDDJ-OEHAA25-4T3KSTM-2MUB5FJ-R6OSZAG"; };
                     "thor" = { id = "ZU3IMAE-WYAKQOF-JQNATOI-EDEYAWR-O4X7SGU-3DC47BX-5FSRJUM-OEWEOQJ"; };
                 };
 
@@ -27,7 +27,7 @@ in
                     "Music" = {
                         id = "7us1i-1qu93";
                         path = "/home/${user}/Music";
-                        devices = [ "desktop" "laptop" "phone" "thor" ];
+                        devices = [ "desktop" "laptop" "phone" "thor" "server" ];
                         ignorePerms = false;
                     };
 
