eff7e6c18479daab8c9c2447ab7c5ade0cc69e75
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-16 07:53:23 +0100

    [Nvim] Updated dashboard sections

--- modules/home/terminal/editors/nvim/lua/main/snacks/dashboard.lua
+++ modules/home/terminal/editors/nvim/lua/main/snacks/dashboard.lua
@@ -1,6 +1,6 @@
 return {
     enabled = true,
-    width = 50,
+    width = 60,
     pane_gap = 4,
     preset = {
         keys = {
@@ -17,22 +17,35 @@ return {
     sections = {
         { section = "header" },
         {
-          pane = 2,
-          icon = " ",
-          title = "Git Status",
-          section = "terminal",
-          enabled = function()
-            return Snacks.git.get_root() ~= nil
-          end,
-          cmd = "git status --short --branch --renames",
-          height = 5,
-          padding = 1,
-          ttl = 5 * 60,
-          indent = 3,
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+                return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
         },
         { section = "keys", gap = 1, padding = 1 },
         { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        {
+            pane = 2,
+            icon = " ",
+            title = "Git Log",
+            section = "terminal",
+            enabled = function()
+                return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git log -n 5 --oneline",
+            height = 9,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+        },
     },
 
 }
