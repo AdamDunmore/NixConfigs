4c03a11ecd6be99706f5708c1f8f0bffddd966a8
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-23 15:40:57 +0100

    [Host] Added webserver to server

--- users/adam/hosts/server/home.nix
+++ users/adam/hosts/server/home.nix
@@ -1,4 +1,7 @@
-{ inputs, system, ... }:
+{ inputs, system, pkgs, ... }:
+let
+    go-markdown-server = import ../../../../pkgs/go-markdown-server.nix { inherit pkgs; };
+in
 {
     imports = [ 
         ../../home.nix 
@@ -6,5 +9,33 @@
     ];
     config = {
         home.apps = [ inputs.webserver.packages.${system}.default ]; 
+
+        services.webserver = {
+            enable = true;
+            environmentVariables = {
+                WEBSERVER_IP = "100.99.196.76";
+                WEBSERVER_PORT = "1913";
+            };
+            # TODO add a way to pass a password secret
+        };
+
+        systemd.user.services.go-markdown-server = {
+            Unit = {
+                Description = "Service for go-markdown-server";
+                After = [ "network-online.target" ];
+            };
+
+            Service = {
+                ExecStart = "${go-markdown-server}/bin/go-markdown-server";
+                Restart = "on-failure";
+                RestartSec = 5;
+                Environment = {
+                    PORT = "9090";
+                    CONTENT_DIR="/home/adam/Notes/";
+                };
+            };
+
+            Install.WantedBy = [ "default.target" ];
+        };
     };
 }
